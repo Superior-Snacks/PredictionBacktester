ad2e3192e918ae20049bb72790c680f9fac970e9
Superior-Snacks <joningiwork@gmail.com>
2026-02-18 17:18:39 +0000

    s

---
+++ PredictionBacktester.Engine/DipBuyingStrategy.cs
@@ -0,0 +1,23 @@
+﻿using PredictionBacktester.Core.Entities.Database;
+
+namespace PredictionBacktester.Engine;
+
+public class DipBuyingStrategy : IStrategy
+{
+    public void Execute(Trade tick, SimulatedBroker broker)
+    {
+        // If we have no shares and the price dips below 40 cents, BUY $100 worth!
+        if (broker.PositionShares == 0 && tick.Price < 0.40m)
+        {
+            broker.Buy(tick.Price, 100m);
+            Console.WriteLine($"[BUY] Bought shares at ${tick.Price}. Remaining Cash: ${broker.CashBalance:F2}");
+        }
+
+        // If we hold shares and the price spikes above 60 cents, SELL EVERYTHING!
+        else if (broker.PositionShares > 0 && tick.Price > 0.60m)
+        {
+            broker.SellAll(tick.Price);
+            Console.WriteLine($"[SELL] Sold shares at ${tick.Price}. New Cash: ${broker.CashBalance:F2}");
+        }
+    }
+}
---
+++ PredictionBacktester.Engine/IStrategy.cs
@@ -0,0 +1,11 @@
+﻿using PredictionBacktester.Core.Entities.Database;
+
+namespace PredictionBacktester.Engine;
+
+public interface IStrategy
+{
+    /// <summary>
+    /// Evaluates the current market tick and decides whether to buy or sell via the broker.
+    /// </summary>
+    void Execute(Trade tick, SimulatedBroker broker);
+}
