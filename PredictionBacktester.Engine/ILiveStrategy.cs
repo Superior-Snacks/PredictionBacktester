a61761231eced5c664531f33084122f377485984
Superior-Snacks <joningiwork@gmail.com>
2026-02-24 18:29:24 +0000

    add decouple

---
+++ PredictionBacktester.Engine/ILiveStrategy.cs
@@ -0,0 +1,9 @@
+﻿using PredictionBacktester.Engine;
+
+namespace PredictionBacktester.Engine;
+
+public interface ILiveStrategy
+{
+    // Every live strategy MUST implement this method!
+    void OnBookUpdate(LocalOrderBook book, GlobalSimulatedBroker broker);
+}
